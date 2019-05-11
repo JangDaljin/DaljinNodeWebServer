a44338915542e96da813d2df97fff45fd6036f81
JangDaljin <toyyj15@naver.com>
2019-05-11 10:32:21 +0900

    VER 1.3.2

--- D_passport.js
+++ D_passport.js
@@ -17,67 +17,7 @@ passport.use(new NaverStrategy({
     var user_email = profile.emails[0].value;
     var user_name = profile.displayName;
 
-    if(!user_email || !user_name) {
-        var options = {
-            uri : 'https://nid.naver.com/oauth2.0/token',
-            qs : {
-                grant_type : 'delete',
-                client_id : OAUTH.NAVER.CLIENT_ID,
-                client_secret : OAUTH.NAVER.CLIENT_SECRET,
-                access_token : accessToken,
-                service_provider : 'NAVER'
-            }
-        }
-        
-        request.get(options , function(err , response , body){
-            console.log("[" + user_email + "/" + user_name + "] 필수항목 비동의");
-        });
-        return done(null , null);
-    }
-
-    var D_UserModel = require('./D_database').D_UserModel;
-
-    D_UserModel.findOne({'email':user_email} , (err,user) => {
-        if(err) {
-            console.log('[' + user_email + '] LOGIN ERROR');
-            return done(err , null);
-        }
-        
-        if(!user) {
-            //최초가입
-            console.log('[' + user_email + '] NOT FOUND ID')
-            var UserModel = new D_UserModel(
-                {
-                    email : user_email,
-                    nickname : user_name ,
-                    grade : 'anonymous',
-                    code : '',
-                    max_storage : 1024*1024*1
-                }
-            );
-
-
-            UserModel.save((err)=> {
-                if(err) {
-                    console.log('[' + user_email + '] ADD USER ERROR');
-                    return done(err , null);
-                }
-                else {
-                    D_file.makeDirectory(D_PATH["DOWNLOAD"]+ '/' + user_email).then(
-                        (returnValue) => 
-                        {
-                            console.log('[' + user_email + '] ADD USER -> MAKE DEFAULT DIRECTORY COMPLETE');
-                            return done(null , UserModel);
-                        }
-                    )
-                }
-            });   
-        }
-        else {
-            console.log('[' + user_email + '] LOGIN SUCCESS');
-            return done(null , user);
-        }
-    });
+    NaverLoginProcess(user_email , user_name , accessToken , done);
 }));
 
 passport.use(new NaverTokenStrategy({
@@ -88,72 +28,76 @@ passport.use(new NaverTokenStrategy({
     callbackURL : OAUTH.NAVER.CALLBACK
 } , 
 (accessToken, refreshToken, profile, done) => {
-
     var loginInfo = profile._json.response;
     var user_email = loginInfo['email']
     var user_name = loginInfo['nickname'];
 
+    NaverLoginProcess(user_email , user_name , accessToken , done);
+}));
 
-    if(!user_email || !user_name) {
-        var options = {
-            uri : 'https://nid.naver.com/oauth2.0/token',
-            qs : {
-                grant_type : 'delete',
-                client_id : OAUTH.NAVER.CLIENT_ID,
-                client_secret : OAUTH.NAVER.CLIENT_SECRET,
-                access_token : accessToken,
-                service_provider : 'NAVER'
-            }
+
+var NaverLoginProcess = (user_email , user_name , accessToken , done) => {
+//필수 항목 비동의
+if(!user_email || !user_name) {
+    var options = {
+        uri : 'https://nid.naver.com/oauth2.0/token',
+        qs : {
+            grant_type : 'delete',
+            client_id : OAUTH.NAVER.CLIENT_ID,
+            client_secret : OAUTH.NAVER.CLIENT_SECRET,
+            access_token : accessToken,
+            service_provider : 'NAVER'
         }
-        
-        request.get(options , function(err , response , body){
-            console.log("[" + user_email + "/" + user_name + "] 필수항목 비동의");
-        });
-        return done(null , null);
     }
     
-    var D_UserModel = require('./D_database').D_UserModel;
-
-    D_UserModel.findOne({'email':user_email} , (err,user) => {
-        if(err) {
-            console.log('[' + user_email + '] LOGIN ERROR');
-            return done(err , null);
-        }
-
-        //최초가입
-        if(!user) {
-            console.log('[' + user_email + '] NOT FOUND ID')
-            var UserModel = new D_UserModel(
-                {
-                    email : user_email,
-                    nickname : user_name ,
-                    grade : 'anonymous',
-                    code : '',
-                    max_storage : 1024*1024*1
-                }
-            );
-            UserModel.save((err)=> {
-                if(err) {
-                    console.log('[' + user_email + '] ADD USER ERROR');
-                    return done(err , null);
-                }
-                else {
-                    D_file.makeDirectory(D_PATH["DOWNLOAD"]+ '/' + user_email).then(
-                        (returnValue) => 
-                        {
-                            console.log('[' + user_email + '] ADD USER -> MAKE DEFAULT DIRECTORY COMPLETE');
-                            return done(null , UserModel);
-                        }
-                    )
-                }
-            });   
-        }
-        else {
-            console.log('[' + user_email + '] LOGIN SUCCESS');
-            return done(null , user);
-        }
+    request.get(options , function(err , response , body){
+        console.log("[" + user_email + "/" + user_name + "] 필수항목 비동의");
     });
-}));
+    return done(null , null);
+}
+
+var D_UserModel = require('./D_database').D_UserModel;
+
+D_UserModel.findOne({'email':user_email} , (err,user) => {
+    if(err) {
+        console.log('[' + user_email + '] LOGIN ERROR');
+        return done(err , null);
+    }
+    
+    if(!user) {
+        //최초가입
+        console.log('[' + user_email + '] NOT FOUND ID')
+        var UserModel = new D_UserModel(
+            {
+                email : user_email,
+                nickname : user_name ,
+                grade : 'anonymous',
+                code : '',
+                max_storage : 1024*1024*1
+            }
+        );
+        //신규 저장
+        UserModel.save((err)=> {
+            if(err) {
+                console.log('[' + user_email + '] ADD USER ERROR');
+                return done(err , null);
+            }
+            else {
+                D_file.makeDirectory(D_PATH["DOWNLOAD"]+ '/' + user_email).then(
+                    (returnValue) => 
+                    {
+                        console.log('[' + user_email + '] ADD USER -> MAKE DEFAULT DIRECTORY COMPLETE');
+                        return done(null , UserModel);
+                    }
+                )
+            }
+        });   
+    }
+    else {
+        console.log('[' + user_email + '] LOGIN SUCCESS');
+        return done(null , user);
+    }
+})};
 
 passport.serializeUser(function(user, done) {
     done(null , user);
