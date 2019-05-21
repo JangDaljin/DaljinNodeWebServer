f4eff9e17178395f41314e2bc9ac4c00357f3583
JangDaljin <toyyj15@naver.com>
2019-05-21 14:14:05 +0900

    TEST

--- views/scripts/daljincloud.js
+++ views/scripts/daljincloud.js
@@ -161,6 +161,7 @@ $(document).ready(function () {
                 alert("폴더 업로드 불가");
                 return;
             }
+            console.log("??FFF");
             alert(files.length + "??B");
             upload(files);
         }else{
