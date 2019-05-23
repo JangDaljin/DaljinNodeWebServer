e4372832f3894994ccb774a44d7ba43b713960e6
JangDaljin <toyyj15@naver.com>
2019-05-23 14:09:19 +0900

    TEST

--- views/scripts/daljincommon.js
+++ views/scripts/daljincommon.js
@@ -53,39 +53,41 @@ $(document).ready(function () {
 });
 
 var getVolumeSize = function(size , deter) {
-    size += "";
-    size = parseInt(size);
-    if(size / 1024 > 1) {                           
-            return getVolumeSize(size / 1024 , deter+1);  
-    } else {                                        
-        var res = '';                                   
-        switch(deter) {                                 
-                case 0 :                                
-                        res = 'Byte';                              
-                        break;                                  
-                case 1 :                                        
-                        res = 'KB';                             
-                        break;                                  
-                case 2 :                                        
-                        res = 'MB';                             
-                        break;                                  
-                case 3 :                                        
-                        res = 'GB';                             
-                        break;                                  
-                case 4 :                                        
-                        res = 'TB';                             
-                        break;                                 
-                default :                                       
-                        res = "NaN";                            
-                        break;                                  
-        }  
-        if(res=='Byte') {
-                return size.toFixed(0)+res;  
+        if(typeof(size) != "number") {
+                return typeof(size);
         }
-        else {
-                return size.toFixed(2)+res;  
-        }
-    }                                             
+        size = parseInt(size);
+        if(size / 1024 > 1) {                           
+                return getVolumeSize(size / 1024 , deter+1);  
+        } else {                                        
+                var res = '';                                   
+                switch(deter) {                                 
+                        case 0 :                                
+                                res = 'Byte';                              
+                                break;                                  
+                        case 1 :                                        
+                                res = 'KB';                             
+                                break;                                  
+                        case 2 :                                        
+                                res = 'MB';                             
+                                break;                                  
+                        case 3 :                                        
+                                res = 'GB';                             
+                                break;                                  
+                        case 4 :                                        
+                                res = 'TB';                             
+                                break;                                 
+                        default :                                       
+                                res = "NaN";                            
+                                break;                                  
+                }  
+                if(res=='Byte') {
+                        return size.toFixed(0)+res;  
+                }
+                else {
+                        return size.toFixed(2)+res;  
+                }
+        }                                             
 };
 
 
