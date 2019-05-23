2d65348e91df10b321de892a9d1498f20de7af60
JangDaljin <toyyj15@naver.com>
2019-05-23 23:38:40 +0900

    TEST

--- views/scripts/daljincalender.js
+++ views/scripts/daljincalender.js
@@ -1,12 +1,42 @@
 $(document).ready(function () {
     
     var date = new Date();
-    showCalender($('.inmonth') , date.getFullYear() , date.getMonth()+1);
 
+    var year = date.getFullYear()
+    var month = date.getMonth()
+
+    showTitle($('.in-calender') , year , month +1);
+    showCalender($('.in-month-calender') , year , month+1);
+    
+
+
+    if(month == 0) {
+        showTitle($('.pre-calender') , year-1 , 1);
+        showCalender($('.pre-month-calender') , year-1 , 1);
+    }
+    else {
+        showTitle($('.pre-calender') , year , month);
+        showCalender($('.pre-month-calender') , year , month);
+    }
+
+    if(month == 11) {
+        showTitle($('.post-calender') , year+1 , month1);
+        showCalender($('.post-month-calender') , year+1 , 1);
+    }
+    else {
+        showTitle($('.post-calender') , year , month+2);
+        showCalender($('.post-month-calender') , year , month+2);
+    }
 });
 
 var day31Month = new Array(1 , 3 , 5 , 7 , 8 , 10 , 12);
 var day30Month = new Array(4, 6 , 9 ,11);
+
+
+var showTitle = function(elem , year , month) {
+    elem.children('.year').children('span').html(year);
+    elem.children('.month').children('span').html(month);
+}
 
 var showCalender = function(elem, year , month) {
     var date = new Date(year + '-' + month + '-1');
@@ -32,30 +62,34 @@ var showCalender = function(elem, year , month) {
         alert('error');
     }
 
-
-
     var tr_cnt = 0;
     res +=  "<table><thead>";
-    res +=  "<th><div>일</div></th>";
+    res +=  "<th class='sun'><div>일</div></th>";
     res +=  "<th><div>월</div></th>";
     res +=  "<th><div>화</div></th>";
     res +=  "<th><div>수</div></th>";
     res +=  "<th><div>목</div></th>";
     res +=  "<th><div>금</div></th>";
-    res +=  "<th><div>토</div></th>";
+    res +=  "<th class='sat'><div>토</div></th>";
     res +=  "</thead><tbody>";
     if(offsetDay != 0) {
         res += "<tr>";
     }
     for(var i = 0; i < offsetDay; i++) {
-        res += "<td></td>";
+        res += "<td class='not-use'></td>";
         tr_cnt++;
     }
     for(var i = 1; i <= max_day; i++) {
         if(tr_cnt == 0) {
             res += "<tr>";
+            res += "<td class='sun'>";
         }
-        res += "<td>";
+        else if(tr_cnt == 6) {
+            res += "<td class='sat'>";
+        }
+        else {
+            res += "<td>";
+        }
         res += '<div class="calender-date">' + i +'</div>';
         res += '<div class="calender-content"><div>';
         res += "</td>";
@@ -65,7 +99,13 @@ var showCalender = function(elem, year , month) {
             res += "</tr>";
             tr_cnt = 0;
         }
+    }
 
+    if(tr_cnt != 7) {
+        for(var i = 0 ; i < 7 - tr_cnt; i++) {
+            res += "<td class='not-use'></td>";
+        }
+        res += "</tr>";
     }
     res += "</tbody></table>";
 
