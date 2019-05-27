2c880574de94f63fecb20dffabc3a0f64086c965
JangDaljin <toyyj15@naver.com>
2019-05-27 13:39:40 +0900

    TEST

--- views/scripts/daljincommon.js
+++ views/scripts/daljincommon.js
@@ -26,6 +26,11 @@ $(document).ready(function () {
                 }
         });
 
+        //NAVER LOGIN
+        $('#naver_login').click(function (e) {
+                window.location.href = "naverlogin?history=" + window.location.pathname;
+        });
+
         //LOGOUT
         $('#btn_logout').click(function (e) {
                 $.ajax({
